1eda32f68b540ec4a9806e5cc9274bb9d07de860
staakaar <vancyroletaka@gmail.com>
2023-09-17 18:11:36 +0900

    database setting info custom async configuration

--- config/database.ts
+++ config/database.ts
@@ -1,26 +1,20 @@
-import { DataSource } from 'typeorm';
 import { ConfigService } from '@nestjs/config';
+import { Injectable } from '@nestjs/common';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
 
-export const databaseConfig = [
-    {
-        provide: 'DATA_SOURCE',
-        useFactory: async () => {
-            const configService = new ConfigService();
-            const dataSource = new DataSource({
-                type: 'postgres',
-                host: 'localhost',
-                port: 5432,
-                username: 'admin',
-                password: 'admin',
-                database: 'test',
-                entities: [
-                    __dirname + '/../**/*.entity{.ts,.js}',
-                ],
-                synchronize: true,
-            });
-
-            return dataSource.initialize();
-            }
-        },
-    },
-];
+@Injectable()
+export class TypeOrmConfigService implements TypeOrmOptionsFactory {
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    const configService = new ConfigService();
+    return {
+      type: 'mysql',
+      host: configService.get('HOST'),
+      port: configService.get('DATABASE_PORT'),
+      username: configService.get('DATABASE_USER'),
+      password: configService.get('DATABASE_PASSWORD'),
+      database: configService.get('POSTGRES_DB'),
+      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      synchronize: true,
+    };
+  }
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from 'config/database';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'admin',
-      password: 'admin',
-      database: 'bff_app',
-      entities: [],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useExisting: TypeOrmConfigService,
     }),
   ],
   controllers: [AppController],
