613c3bd8af13323ba3209dd99acbb0396d9b400b
staakaar <vancyroletaka@gmail.com>
2023-09-18 21:14:40 +0900

    fix import entities

--- src/article/article.module.ts
+++ src/article/article.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Article } from 'src/article/article';
+import { Article } from 'src/article/article.entities';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Article])],
