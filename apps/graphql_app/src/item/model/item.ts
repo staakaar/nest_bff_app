fb3d639bd26c5e3f12618c3b68254e97ef092c23
staakaar <vancyroletaka@gmail.com>
2023-09-23 16:53:30 +0900

    add item fields

--- apps/graphql_app/src/item/model/item.ts
+++ apps/graphql_app/src/item/model/item.ts
@@ -1,4 +1,25 @@
-import { ObjectType } from "@nestjs/graphql";
+import { Field, Int, ObjectType } from '@nestjs/graphql';
 
-@ObjectType
-export class Item {}
+@ObjectType()
+export class Item {
+  @Field(type => Int)
+  id: number;
+
+  @Field(type => Int)
+  itemNo: number;
+
+  @Field()
+  name: string;
+
+  @Field(type => BigInt)
+  quantity: number;
+
+  @Field()
+  unit: string;
+
+  @Field(type => BigInt)
+  price: number;
+
+  @Field(type => String)
+  remark: string;
+}
