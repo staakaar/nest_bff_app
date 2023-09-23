0c70c80bb86f4714c48620a9451e5316c108ac6a
staakaar <vancyroletaka@gmail.com>
2023-09-23 21:30:08 +0900

    graghql schema first

--- apps/graphql_app/src/graphql_app.module.ts
+++ apps/graphql_app/src/graphql_app.module.ts
@@ -4,11 +4,16 @@ import { GraphqlAppService } from './graphql_app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { UserService } from './user/user.service';
+import { join } from 'path';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
+      typePaths: ['./**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/schema.ts'),
+      },
     }),
   ],
   controllers: [GraphqlAppController],
