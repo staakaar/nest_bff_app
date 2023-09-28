f2c832922d4f67fa24f3873e0652a8660db18395
staakaar <vancyroletaka@gmail.com>
2023-09-29 00:17:01 +0900

    edit typeOrmModule

--- apps/graphql_app/src/graphql_app.module.ts
+++ apps/graphql_app/src/graphql_app.module.ts
@@ -3,16 +3,16 @@ import { GraphqlAppController } from './graphql_app.controller';
 import { GraphqlAppService } from './graphql_app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { TypeOrmConfigService } from './database';
 import { UserService } from './user/user.service';
 import { join } from 'path';
-import source from 'apps/nest_bff_app/src/config/ormconfig';
 import { UserModule } from './user/user.module';
 import { ItemModule } from './item/item.module';
 import { ConfigModule } from '@nestjs/config';
 import { ItemController } from './item/item.controller';
 import { ItemService } from './item/item.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Item } from './item/item.entity';
+import { ItemResolver } from './item/item.resolver';
 
 @Module({
   imports: [
@@ -22,18 +22,20 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       autoSchemaFile: join(process.cwd(), '/apps/graphql_app/src/schema.gql'),
       sortSchema: true,
     }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useClass: TypeOrmConfigService,
-      dataSourceFactory: async () => {
-        const dataSource = await source.initialize();
-        return dataSource;
-      },
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'admin',
+      password: 'admin',
+      database: 'graghql_app',
+      synchronize: true,
+      entities: [Item],
     }),
     ItemModule,
     UserModule,
   ],
   controllers: [GraphqlAppController, ItemController],
-  providers: [GraphqlAppService, UserService, ItemService],
+  providers: [GraphqlAppService, UserService, ItemService, ItemResolver],
 })
 export class GraphqlAppModule {}
